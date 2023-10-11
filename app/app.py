f06a1e3de3991ced6fa65b9b3a9d45a09f596afb
Santiago <Kuja2022@gmail.com>
2023-10-11 12:34:02 +0200

    png y ajustes markdown, streamlit

--- app/app.py
+++ app/app.py
@@ -7,22 +7,35 @@ git_clone = "Referencia a git clone"
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
 # Inicio de git, opciones y menus
-def main(): 
-    st.title("Taller de Git Hub 2023")
-    st.image(os.path.join(dir_path,"..","img","git_image.jpg"),width=650)
+def main():
+    st.markdown("<h1 style='text-align: center; color: black;'>Taller de Git Hub 2023</h1>", unsafe_allow_html=True) 
+    st.image(("../img/git_image.jpg"))
+    # st.image(os.path.join(dir_path,"..","img","git_image.jpg"),width=650) Para deploy
     page = st.sidebar.selectbox("Selecciona una página", ("Información principal de Git Hub", "Comandos básicos de Git Hub", "Enlaces a repositorios"))
     
     def info_main():
-        st.title ("Información principal de git hub")
+        st.markdown ("<h2 style='text-align: center; color: black;'>Información principal de Git Hub</h2>",unsafe_allow_html=True)
         git_bas = st.checkbox("¿Que es Git?, ¿Que es Git Hub?, Diferencias entre ambas")
         if git_bas:
-            st.write("Todo lo relacionado con git al inicio.")
+            st.markdown("###### - Git es un sistema de control de versiones distribuido. Es una herramienta que permite a los desarrolladores rastrear los cambios en su código fuente a lo largo del tiempo.")
+            st.markdown("###### - GitHub, por otro lado, es una plataforma en línea que utiliza Git como motor subyacente.")
+            st.markdown("###### - En resumen, Git es el sistema de control de versiones que te permite rastrear los cambios en tu código en tu computadora local, mientras que GitHub es una plataforma en línea que utiliza Git para alojar repositorios en la nube, facilitando la colaboración y la gestión de proyectos de software.")
     def com_bas():
-        st.title ("Comandos basicos de Git Hub")
-        st.markdown("#### En este apartado tendras acceso a los comandos básico de git hub")
+        st.markdown ("<h2 style='text-align: center; color: black;'>Comandos basicos de Git Hub</h2>",unsafe_allow_html=True)
+        st.markdown("#### En este apartado tendras acceso a los comandos básicos de git hub")
         option = st.selectbox(("¿Que comando quieres utilizar?"),("ADD","Commit","Clone"),index=None,placeholder="Seleccione el comando")
+        if option == "Clone":
+            st.markdown ("<h2 style='text-align: center; color: black;'>Git Clone</h2>",unsafe_allow_html=True)
+            st.markdown("#### Es un comando que permite realizar una copia idéntica de la última versión de un proyecto y la guarda en tu ordenador")
+            st.markdown("###### - Primero entra en el repositorio y haz click en code")
+            st.image(("../img/git_clone.png"))
+            # st.image(os.path.join(dir_path,"..","img","git_clone.png"),width=650) Para deploy
+            st.markdown("###### - Después haz click en el icono indicado y copia el enlace")
+            st.image(("../img/git_clone_2.png"))
+            st.markdown("###### Primero antes de hacer el git clone debes añadir un espacio de trabajo nuevo, seleccionas una carpeta que esté vacia para tu espacio de trabajo y en la consola del VSC escribe git clone <link-del-repositorio>")
+            st.image(("../img/git_clone_3.png"))
     def link_repo():
-        st.title ("Enlaces a repositorios")
+        st.markdown ("<h2 style='text-align: center; color: black;'>Enlaces a repositorios</h2>",unsafe_allow_html=True)
         st.markdown ("##### Escoger el enlace correspondiente a tu nombre y aplicar un git clone al repositorio adecuado.")
         st.markdown ("###### (Solo os voy a dar acceso al repositorio indicado por lo que solo podreis acceder al repositorio que tiene vuestro nombre)")
         # Botones de enlace a cada repositorio 
@@ -47,7 +60,6 @@ def main():
         st.link_button("Repositorio de Raimundo","https://github.com/TiagoValen/githubtaller_raimundo")
         st.link_button("Repositorio de Rodrigo","https://github.com/TiagoValen/githubtaller_rodrigo")
         st.link_button("Repositorio de Sara","https://github.com/TiagoValen/githubtaller_sara")
-        st.link_button("Repositorio de Sergio","https://github.com/TiagoValen/githubtaller_sergio")
         st.link_button("Repositorio de Steven","https://github.com/TiagoValen/githubtaller_steven")
     
     # Configuración de la página y de menu de selección de taller
